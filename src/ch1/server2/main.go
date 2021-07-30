01d3b44fd8dcb473413b1fef66b911da829cc8cc
Tácio <taciogt@gmail.com>
2021-07-29 23:02:32 -0300

    ch1/server2

--- src/ch1/server2/main.go
+++ src/ch1/server2/main.go
@@ -8,14 +8,29 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync"
 )
 
+var mu sync.Mutex
+var count int
+
 func main() {
-	http.HandleFunc("/", handler) // each request call handler
+	http.HandleFunc("/", handler)      // each request call handler
+	http.HandleFunc("/count", counter) // each request call handler
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
 // handler echoes the Path component of the requested URL.
 func handler(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	count++
+	mu.Unlock()
 	_, _ = fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+}
+
+// counter echoes the number of calls so far.
+func counter(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	fmt.Fprintf(w, "Count %d\n", count)
+	mu.Unlock()
 }
