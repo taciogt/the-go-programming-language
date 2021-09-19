4ed568e313a84e6b194f110c7d269ebf71fd4d5d
Tácio <taciogt@gmail.com>
2021-09-18 22:28:07 -0300

    ch2/exercise04

---
+++ src/ch2/exercise04/main_test.go
@@ -0,0 +1,28 @@
+// How to run the benchmarks: go test -bench=. ./exercise03
+package main
+
+import "testing"
+
+func BenchmarkPopCount(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for j := uint64(0); j < 256; j++ {
+			PopCount(j)
+		}
+	}
+}
+
+func BenchmarkLoopedPopCount(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for j := uint64(0); j < 256; j++ {
+			LoopedPopCount(j)
+		}
+	}
+}
+
+func BenchmarkPopCount64Shift(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for j := uint64(0); j < 256; j++ {
+			PopCount64Shift(j)
+		}
+	}
+}
