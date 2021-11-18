889cb775e2d1fb0829e26d0c8b923d0ef2553c37
Tácio <taciogt@gmail.com>
2021-11-17 22:14:11 -0300

    ch3/basename2

---
+++ src/ch3/basename2/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	fmt.Println(basename("a/b/c.go"))
+	fmt.Println(basename("c.d.go"))
+	fmt.Println(basename("abc"))
+}
+
+// basename removes directory components and a .suffix.
+func basename(s string) string {
+	slash := strings.LastIndex(s, "/") // -1 if "/" not found
+	s = s[slash+1:]
+	if dot := strings.LastIndex(s, "."); dot >= 0 {
+		s = s[:dot]
+	}
+	return s
+}
