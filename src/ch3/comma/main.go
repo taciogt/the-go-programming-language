a1c444c4cc25a0eaeb7366b1b6707d813c0b1e3b
Tácio <taciogt@gmail.com>
2021-11-17 22:29:07 -0300

    ch3/commaForFloats

--- src/ch3/comma/main.go
+++ src/ch3/comma/main.go
@@ -1,11 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	nums := []int{1, 12, 123, 1234, 12345, 123456, 1234567}
 	for _, n := range nums {
 		fmt.Printf("%d: %s\n", n, comma(fmt.Sprintf("%d", n)))
+	}
+
+	fStrings := []string{"1", "12", "123.45", "1222.3333"}
+	for _, n := range fStrings {
+		fmt.Printf("%s: %s\n", n, commaForFloat(n))
 	}
 }
 
@@ -16,4 +24,12 @@ func comma(s string) string {
 		return s
 	}
 	return comma(s[:n-3]) + "," + s[n-3:]
+}
+
+func commaForFloat(s string) string {
+	i := strings.Index(s, ".")
+	if i == -1 {
+		return comma(s)
+	}
+	return comma(s[:i]) + s[i:]
 }
