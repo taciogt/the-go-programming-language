be885e23a3ebbffb81ae5853f4ac689d2edba47c
Tácio Tavares <taciogt@gmail.com>
2021-12-30 21:42:36 -0300

    ch4/append

---
+++ src/ch4/append/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x, y []int
+	for i := 0; i < 10; i++ {
+		y = appendInt(x, i)
+		fmt.Printf("%d  cap=%d\t%v\n", i, cap(y), y)
+		x = y
+	}
+	y = appendInt(y, y...)
+	fmt.Printf("variadic append  cap=%d\t%v\n", cap(y), y)
+	x = y
+}
+
+func appendInt(x []int, y ...int) []int {
+	var z []int
+	zlen := len(x) + len(y)
+	if zlen <= cap(x) {
+		// There is room to grow. Extend the slice.
+		z = x[:zlen]
+	} else {
+		// There is insufficient space. Allocate a new array.
+		// Grow by doubling, for amortized liner complexity.
+		zcap := zlen
+		if zcap < 2*len(x) {
+			zcap = 2 * len(x)
+		}
+		z = make([]int, zlen, zcap)
+		copy(z, x) // a built-in function; see text
+	}
+	copy(z[len(x):], y)
+	return z
+}
