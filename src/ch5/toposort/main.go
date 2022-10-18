ac8a575ede5923b8ea85759ca071cf641cd6e61a
Tácio Tavares <taciogt@gmail.com>
2022-10-17 22:01:37 -0300

    ch5/toposort-p1

---
+++ src/ch5/toposort/main.go
@@ -0,0 +1,18 @@
+package main
+
+var prereqs = map[string][]string{
+	"algorithms":            {"data structures"},
+	"calculus":              {"linear algebra"},
+	"compilers":             {"data structures", "formal languages", "computer organization"},
+	"data structures":       {"discrete math"},
+	"databases":             {"data structures"},
+	"discrete math":         {"intro do programming"},
+	"formal languages":      {"discrete math"},
+	"networks":              {"operating systems"},
+	"operating systems":     {"data structures", "computer organization"},
+	"programming languages": {"data structures", "computer organization"},
+}
+
+func main() {
+
+}
